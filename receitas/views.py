a1c2b070963106c64e49f5cc9c5a66a2433e982e
ggbruzzi <guilhermebruzzi20@gmail.com>
2022-12-16 00:11:00 -0300

    último commit da segunda parte

--- receitas/views.py
+++ receitas/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Receita
 
-
 def index(request):
-
     receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
 
     dados = {
         'receitas' : receitas
     }
-    return render(request, 'index.html',dados)
+    return render(request,'index.html', dados)
 
 def receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id)
@@ -18,14 +16,14 @@ def receita(request, receita_id):
         'receita' : receita
     }
 
-    return render(request, 'receita.html', receita_a_exibir)
+    return render(request,'receita.html', receita_a_exibir)
 
 def buscar(request):
     lista_receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
 
     if 'buscar' in request.GET:
         nome_a_buscar = request.GET['buscar']
-        if nome_a_buscar:
+        if buscar:
             lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
 
     dados = {
